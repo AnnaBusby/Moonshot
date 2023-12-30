b5a3ada8da9b72a808434a40664a4c06c999c685
Anna Busby <anna.busby.services@outlook.com>
2023-12-30 18:34:27 +0000

    Scroll view

--- Moonshot/Practice.swift
+++ Moonshot/Practice.swift
@@ -7,19 +7,44 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct Practice: View {
     var body: some View {
         Image(.example)
             .resizable()
             .scaledToFit()
-            .frame(width: 300, height: 300)
+            .frame(width: 300, height: 100)
             
         Image(.example)
             .resizable()
             .scaledToFit()
+        // this with resize the image correctly to a set percentage of the avaible screen space
             .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
+                size * 0.5
             }
+        
+        ScrollView {
+            //the lazy will make the view as needed, so as the user scrolls the view will be created
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText(text: "Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
