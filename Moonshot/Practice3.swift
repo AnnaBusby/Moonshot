96d75ecdb92ae07dbdb91ada36ebf45a881097bb
Anna Busby <anna.busby.services@outlook.com>
2023-12-30 20:27:02 +0000

    Decoding

---
+++ Moonshot/Practice3.swift
@@ -0,0 +1,18 @@
+//
+//  Practice3.swift
+//  Moonshot
+//
+//  Created by Anna Busby on 30/12/2023.
+//
+
+import SwiftUI
+
+struct Practice3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Practice3()
+}
