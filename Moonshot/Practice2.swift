503d044194d6c255c38b97a60ffae5c31d6d0696
Anna Busby <anna.busby.services@outlook.com>
2023-12-30 18:45:26 +0000

    New views using navigation link

---
+++ Moonshot/Practice2.swift
@@ -0,0 +1,18 @@
+//
+//  Practice2.swift
+//  Moonshot
+//
+//  Created by Anna Busby on 30/12/2023.
+//
+
+import SwiftUI
+
+struct Practice2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Practice2()
+}
