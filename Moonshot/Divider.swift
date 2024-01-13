2280a130b56ee2cfee6811b8a09d5e1e0d040c6d
Anna Busby <anna.busby.services@outlook.com>
2024-01-13 14:03:47 +0000

    D42 - challenge 2, separte ui code into a new view

---
+++ Moonshot/Divider.swift
@@ -0,0 +1,18 @@
+//
+//  Divider.swift
+//  Moonshot
+//
+//  Created by Anna Busby on 13/01/2024.
+//
+
+import SwiftUI
+
+struct Divider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Divider()
+}
